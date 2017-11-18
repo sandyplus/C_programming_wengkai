db3f682e3bc4c7a73327b7c6efd60b97e104cf8c
sandyplus <twiliochina@gmail.com>
2017-11-18 16:09:16 +0800

    CONSTANT VALUES

--- Chapter1/change.c
+++ Chapter1/change.c
@@ -7,7 +7,10 @@ int main()
 	printf("请输入金额（元）");
 	scanf("%d",&price);
 	
-	int change = 100 - price;
+	//int change = 100 - price;
+	const int AMOUNT = 100; //不要有magic number, 一般constant值用大写
+	int change = AMOUNT - price;
+
 	printf("找您%d元\n",change);
 
 	return 0;	
